805bc8c7670a0aec57b15c13558f249cb0e49ac5
codewithshamim <shamimislamonline@gmail.com>
2022-05-15 18:28:47 +0600

    create booking api

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// use middleware
+// _________use middleware__________
 app.use(cors());
 app.use(express.json());
 
@@ -14,10 +14,12 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6xpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
 async function run() {
     try {
         await client.connect();
         const treatmentsCollection = client.db('doctors_portal').collection('treatments');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
 
         // ______get treatment service______
         app.get('/treatment', async(req, res) => {
@@ -26,7 +28,17 @@ async function run() {
             const treatments = await cursor.toArray();
             res.send(treatments);
         })
-    } finally {}
+
+        // _________create booking api________
+        app.post('/booking', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
