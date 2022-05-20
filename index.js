c541a6eaf518b2737ec2c5a0b8ba8f77b8c7c66c
codewithshamim <shamimislamonline@gmail.com>
2022-05-20 20:43:54 +0600

    get all user api created

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.use(cors());
 app.use(express.json());
 
 
+// _____create middleware for verify jwt token_________
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized access" });
+    };
+
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+
+    });
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x6xpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -73,12 +92,18 @@ async function run() {
         })
 
         // get per user booking 
-        app.get('/booking', async(req, res) => {
+        app.get('/booking', verifyJwt, async(req, res) => {
             const patientEmail = req.query.email;
+            const decodedEmail = req.decoded.email;
             // console.log(patientEmail)
-            const query = { patientEmail: patientEmail };
-            const result = await bookingCollection.find(query).toArray();
-            res.send(result);
+            if (patientEmail === decodedEmail) {
+                const query = { patientEmail: patientEmail };
+                const result = await bookingCollection.find(query).toArray();
+                res.send(result);
+            } else {
+                return res.status(403).send({ message: "Forbidden access" })
+            };
+
         });
 
         // put user info 
@@ -89,7 +114,7 @@ async function run() {
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    email: user
+                    email: user.email
                 }
             };
 
@@ -99,6 +124,15 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
             res.send({ result, token });
         })
+
+        // get all user 
+        app.get('/user', verifyJwt, async(req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users)
+        })
+
+        // create admin 
+
 
     } finally {
         // await client.close();
